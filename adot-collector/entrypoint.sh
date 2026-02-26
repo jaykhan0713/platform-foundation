7279b2deebf804bda2f3536c323c0125db197952
jaykhan0713 <jaykhan0713@gmail.com>
2026-02-26 12:40:46 -0800

    removed adot sh usage

--- adot-collector/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-CONFIG="${OTEL_CONFIG_YAML:+env:OTEL_CONFIG_YAML}"
-CONFIG="${CONFIG:-/otel-config.yaml}"
-
-echo "Starting awscollector with config: $CONFIG"
-
-exec /awscollector --config "$CONFIG"
